2f82f75b40feafbee6bbfcd7aeb393922ad3544a
prince kumar <agarwal8789@gmail.com>
2024-09-06 20:40:44 +0530

    commit made on ai companion upadted

---
+++ app/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Seetings = () => {
+    return (
+        <div>
+            Hii, I am Going to implement Stripe Soon
+        </div>
+    )
+}
+
+export default Seetings
